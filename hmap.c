11ad7a777ead8b8330f9175f2a3652651bb39998
pepsi7959 <pepsi_kmutnb@windowslive.com>
2016-07-24 23:30:16 +0700

    Revised data structure

--- hmap.c
+++ hmap.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-#define DEUBUG(...) do {printf( __VA_ARGS__ );}while(0)
+#define DEBUG(...) do {printf( __VA_ARGS__ );}while(0)
 
 #define VALIDATE_DB(_hdb, _key, _klen) do{  \
     if( _hdb == NULL ){                     \
@@ -100,7 +101,6 @@
     _curr = _curr->next;                    \
 }
 
-
 static int
 hmap_cmp(void *v1, int v1_len, void *v2, int v2_len){
     if(v1_len != v2_len){
@@ -189,51 +189,92 @@ int hmap_is_locate(HMAP_DB *hmap_db, void *key, int k_len){
     return hmap_search(hmap_db, key, k_len, NULL);
 }
 
-int hmap_add(HMAP_DB **hmap_db, void *key, int k_len, void *data, int d_len, int dup_flag){
+#define HMAP_SET_DATA(){                                                              \
+  switch( data_type ){                                                                \
+      case HMAP_DATA_TYPE_INT:                                                        \
+          new_tuple->vals.val_int = va_arg(args, int);                                \
+          DEBUG("add[%d] data[%d]\n", index, new_tuple->vals.val_int);                \
+          break;                                                                      \
+      case HMAP_DATA_TYPE_UINT:                                                       \
+          new_tuple->vals.val_uint = va_arg(args, unsigned int);                      \
+          DEBUG("add[%d] data[%u]\n", index, new_tuple->vals.val_uint);               \
+          break;                                                                      \
+      case HMAP_DATA_TYPE_INT64:                                                      \
+          new_tuple->vals.val_int64 = va_arg(args, long int);                         \
+          DEBUG("add[%d] data[%ld]\n", index, new_tuple->vals.val_int64);             \
+          break;                                                                      \
+      case HMAP_DATA_TYPE_UNIT64:                                                     \
+          new_tuple->vals.val_uint64 = va_arg(args, unsigned long int);               \
+          DEBUG("add[%d] data[%lu]\n", index, new_tuple->vals.val_uint64);            \
+          break;                                                                      \
+      case HMAP_DATA_TYPE_DOUBLE:                                                     \
+          new_tuple->vals.val_double = va_arg(args, double);                          \
+          DEBUG("add[%d] data[%f]\n", index, new_tuple->vals.val_double);             \
+          break;                                                                      \
+      case HMAP_DATA_TYPE_CHARS:                                                      \
+          data  = va_arg(args, char *);                                               \
+          d_len = va_arg(args, int);                                                  \
+          new_tuple->vals.val_chars = (char *)calloc(1, d_len+1);                     \
+          if( new_tuple->vals.val_chars == NULL){                                     \
+              free(new_tuple);                                                        \
+              return HMAP_ALLOCATED_ERROR;                                            \
+          }                                                                           \
+          if ( memcpy( new_tuple->vals.val_chars, data, d_len ) == NULL ){            \
+              free(new_tuple->vals.val_chars);                                        \
+              free(new_tuple);                                                        \
+              return  HMAP_FAILED;                                                    \
+          }                                                                           \
+          ((char *)new_tuple->vals.val_chars)[d_len] = 0;                             \
+          new_tuple->data_len = d_len;                                                \
+          DEBUG("add[%d] data[%s]\n", index, (char *)new_tuple->vals.val_chars);      \
+          break;                                                                      \
+      case HMAP_DATA_TYPE_CUSTOM:                                                     \
+          new_tuple->vals.val_custom  = va_arg(args, void *);                         \
+          DEBUG("add[%d] data[%p]\n", index, new_tuple->vals.val_custom);             \
+          break;                                                                      \
+      default :                                                                       \
+        return HMAP_DATA_TYPE_INVALID;                                                \
+  }                                                                                   \
+} 
+
+int hmap_add(HMAP_DB **hmap_db, void *key, int k_len, int dup_flag, int data_type, ...){
     
     VALIDATE_DB(*hmap_db, key, k_len);
     
     TUPLE *new_tuple = NULL, *ptr_tuple = NULL, *root_tuple = NULL ;
-    int r = 0;
+    int r       = 0;
+    int d_len   = 0;
+    char *data  = NULL;
+    va_list args;
+
     int index = hash(key, k_len) % (*hmap_db)->bucket_size;
-    
+    va_start(args, data_type);
     r = hmap_search(*hmap_db, key, k_len, &ptr_tuple);
     
     if ( r == HMAP_SUCCESS ){
         return HMAP_TUPLE_ALREADY_EXIST;
     }
     
-    if ( r  == HMAP_TUPLE_NOT_FOUND ){
+    if ( r  == HMAP_TUPLE_NOT_FOUND ){ /* there are avialable record to store */
     
         root_tuple = ((*hmap_db)->tuple + index);
         
         if( root_tuple == NULL ){
             return HMAP_TUPUL_EMPTY;
         }
-        
-        if(root_tuple->key_len == 0){
-        
-            root_tuple->data = (char *)calloc(1, d_len+1);
+
+        if(root_tuple->key_len == 0){ /* Reuse record */
+            new_tuple = root_tuple;
+
+            DEBUG("Primary :> ");
+            HMAP_SET_DATA();                                                          
             
-            if( root_tuple->data == NULL){
-                return HMAP_ALLOCATED_ERROR;
-            }
-            
-            if ( memcpy( root_tuple->data, data, d_len ) == NULL){
-                free(root_tuple->data);
-                return  HMAP_FAILED;
-            }
-            
-            ((char *)root_tuple->data)[d_len] = 0;
-            root_tuple->data_len = d_len;
-            
-            DEUBUG("add[%d] primary data[%s]\n", index, (char *)root_tuple->data);
-            memcpy(root_tuple->key, key, k_len);
-            root_tuple->key[k_len] = 0;
-            root_tuple->key_len = k_len;
-            root_tuple->index = index;
-            root_tuple->type = HMAP_TUPLE_PRIMARY;
-            
+            memcpy(new_tuple->key, key, k_len);                                                 
+            new_tuple->key[k_len]  = 0;                                                         
+            new_tuple->key_len     = k_len;                                                     
+            new_tuple->index       = index;                                                     
+            new_tuple->type        = HMAP_TUPLE_PRIMARY;
+            new_tuple->data_type   = data_type;
             if( root_tuple->hash_next == NULL && root_tuple->hash_prev == NULL){
                 root_tuple->hash_next = root_tuple->hash_prev = root_tuple;
             }else{
@@ -243,36 +284,23 @@ int hmap_add(HMAP_DB **hmap_db, void *key, int k_len, void *data, int d_len, int
            
             
         }else{
-            new_tuple = (TUPLE *) calloc(1, sizeof(TUPLE));
+            new_tuple = (TUPLE *) calloc(1, sizeof(TUPLE)); /* Create new recordeto store data */
 
             if( new_tuple == NULL ){
                 return HMAP_ALLOCATED_ERROR;
             } 
-            
-            new_tuple->data = (char *)calloc(1, d_len+1);
-            
-            if( new_tuple->data == NULL){
-                free(new_tuple);
-                return HMAP_ALLOCATED_ERROR;
-            }
-            
-            if ( memcpy( new_tuple->data, data, d_len ) == NULL ){
-                free(new_tuple->data);
-                free(new_tuple);
-                return  HMAP_FAILED;
-            }
-            
-            ((char *)new_tuple->data)[d_len] = 0;
-            new_tuple->data_len = d_len;
-            
-            DEUBUG("add[%d] secondary data[%s]\n", index, (char *)new_tuple->data);
+            DEBUG("Secondary :> ");
+            HMAP_SET_DATA();
+   
             memcpy(new_tuple->key, key, k_len);
             new_tuple->key[k_len] = 0;
-            new_tuple->key_len = k_len;
-            new_tuple->index = index;
-            new_tuple->hash_next = new_tuple->hash_prev = NULL;
-            new_tuple->next = new_tuple->prev = NULL;
-            new_tuple->type = HMAP_TUPLE_SECONDARY;
+            new_tuple->key_len    = k_len;
+            new_tuple->index      = index;
+            new_tuple->hash_next  = new_tuple->hash_prev = NULL;
+            new_tuple->next       = new_tuple->prev = NULL;
+            new_tuple->type       = HMAP_TUPLE_SECONDARY;
+            new_tuple->data_type  = data_type;
+
             (*hmap_db)->secondary_tuple_count++;
             HDB_LIST_APPEND((*hmap_db)->list_tuple, new_tuple);
             HDB_HASH_APPEND(root_tuple, new_tuple);
@@ -287,7 +315,7 @@ int hmap_add(HMAP_DB **hmap_db, void *key, int k_len, void *data, int d_len, int
     return index;
 }
 
-int hmap_add_tuple_with_data(HMAP_DB **hmap_db, void *key, int k_len, void *data, int dup_flag){
+int hmap_add_tuple_with_data(HMAP_DB **hmap_db, void *key, int k_len, int data_type, void *data, int dup_flag){
     
     VALIDATE_DB(*hmap_db, key, k_len);
     
@@ -314,7 +342,7 @@ int hmap_add_tuple_with_data(HMAP_DB **hmap_db, void *key, int k_len, void *data
             root_tuple->data = data;
             root_tuple->data_len = -1;
             
-            DEUBUG("add[%d] primary data[%s]\n", index, (char *)root_tuple->data);
+            DEBUG("add[%d] primary data[%s]\n", index, (char *)root_tuple->data);
             memcpy(root_tuple->key, key, k_len);
             root_tuple->key[k_len] = 0;
             root_tuple->key_len = k_len;
@@ -339,7 +367,7 @@ int hmap_add_tuple_with_data(HMAP_DB **hmap_db, void *key, int k_len, void *data
             new_tuple->data = data;
             new_tuple->data_len = -1;
             
-            DEUBUG("add[%d] secondary data[%s]\n", index, (char *)new_tuple->data);
+            DEBUG("add[%d] secondary data[%s]\n", index, (char *)new_tuple->data);
             memcpy(new_tuple->key, key, k_len);
             new_tuple->key[k_len] = 0;
             new_tuple->key_len = k_len;
@@ -425,8 +453,30 @@ static void hmap_print( HMAP_DB *my_hmap_db ){
     int i = 0;
     TUPLE * my_tuple = NULL;
     my_tuple = my_hmap_db->tuple;
-    for(; i < my_hmap_db->bucket_size; i++){
-        printf("index[%d][%p] key[%s], data[%s]\n", i,my_tuple , my_tuple->key, (char *)my_tuple->data);
+    for(i = 0; i < my_hmap_db->bucket_size; i++){
+        switch( my_tuple->data_type ){
+          case HMAP_DATA_TYPE_INT:
+            printf("index[%d][%p] key[%s], data[%d]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_int);
+            break;
+          case HMAP_DATA_TYPE_UINT:
+            printf("index[%d][%p] key[%s], data[%u]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_uint);
+            break;
+          case HMAP_DATA_TYPE_INT64:
+            printf("index[%d][%p] key[%s], data[%ld]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_int64);
+            break;
+          case HMAP_DATA_TYPE_UNIT64:
+            printf("index[%d][%p] key[%s], data[%lu]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_uint64);
+            break;
+          case HMAP_DATA_TYPE_DOUBLE:
+            printf("index[%d][%p] key[%s], data[%f]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_double);
+            break;
+          case HMAP_DATA_TYPE_CHARS:
+            printf("index[%d][%p] key[%s], data[%s]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_chars);
+            break;
+          case HMAP_DATA_TYPE_CUSTOM:
+            printf("index[%d][%p] key[%s], data[%p]\n", i, my_tuple , my_tuple->key, my_tuple->vals.val_custom);
+            break;
+        }
         my_tuple++;
     }
 }
@@ -434,9 +484,29 @@ static void hmap_print( HMAP_DB *my_hmap_db ){
 static void hmap_print_list_tuple( HMAP_DB *my_hmap_db ){
     TUPLE *ptr_list_tuple = my_hmap_db->list_tuple;
     while( ptr_list_tuple ){
-        
-        printf("index[%d][%p] key[%s], data[%s]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, (char *)ptr_list_tuple->data);
-
+        switch( ptr_list_tuple->data_type ){
+          case HMAP_DATA_TYPE_INT:
+            printf("index[%d][%p] key[%s], data[%d]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_int);
+            break;
+          case HMAP_DATA_TYPE_UINT:
+            printf("index[%d][%p] key[%s], data[%u]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_uint);
+            break;
+          case HMAP_DATA_TYPE_INT64:
+            printf("index[%d][%p] key[%s], data[%ld]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_int64);
+            break;
+          case HMAP_DATA_TYPE_UNIT64:
+            printf("index[%d][%p] key[%s], data[%lu]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_uint64);
+            break;
+          case HMAP_DATA_TYPE_DOUBLE:
+            printf("index[%d][%p] key[%s], data[%f]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_double);
+            break;
+          case HMAP_DATA_TYPE_CHARS:
+            printf("index[%d][%p] key[%s], data[%s]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_chars);
+            break;
+          case HMAP_DATA_TYPE_CUSTOM:
+            printf("index[%d][%p] key[%s], data[%p]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_custom);
+            break;
+        }
         if( ptr_list_tuple->next == my_hmap_db->list_tuple){
             break;
         }
@@ -446,21 +516,43 @@ static void hmap_print_list_tuple( HMAP_DB *my_hmap_db ){
 
 static void hmap_print_tree_tuple( HMAP_DB *my_hmap_db ){
     int i = 0;
-    TUPLE *root_tuple, *ptr_tuple;
+    TUPLE *root_tuple, *ptr_list_tuple;
     for( ; i < my_hmap_db->bucket_size ; i++){
-        ptr_tuple = root_tuple = (my_hmap_db->tuple+i);
+        ptr_list_tuple = root_tuple = (my_hmap_db->tuple+i);
         if( root_tuple == NULL  ){
             continue;
         }
-        printf("index[%d]\n", ptr_tuple->index);
-        while( ptr_tuple ){
-            if ( ptr_tuple->key_len > 0){
-                printf("\t[%p] key[%s], data[%s]\n", ptr_tuple,  ptr_tuple->key, (char *)ptr_tuple->data);
+        printf("index[%d]\n", ptr_list_tuple->index);
+        while( ptr_list_tuple ){
+            if ( ptr_list_tuple->key_len > 0){
+              switch( ptr_list_tuple->data_type ){
+                case HMAP_DATA_TYPE_INT:
+                  printf("index[%d][%p] key[%s], data[%d]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_int);
+                  break;
+                case HMAP_DATA_TYPE_UINT:
+                  printf("index[%d][%p] key[%s], data[%u]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_uint);
+                  break;
+                case HMAP_DATA_TYPE_INT64:
+                  printf("index[%d][%p] key[%s], data[%ld]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_int64);
+                  break;
+                case HMAP_DATA_TYPE_UNIT64:
+                  printf("index[%d][%p] key[%s], data[%lu]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_uint64);
+                  break;
+                case HMAP_DATA_TYPE_DOUBLE:
+                  printf("index[%d][%p] key[%s], data[%f]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_double);
+                  break;
+                case HMAP_DATA_TYPE_CHARS:
+                  printf("index[%d][%p] key[%s], data[%s]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_chars);
+                  break;
+                case HMAP_DATA_TYPE_CUSTOM:
+                  printf("index[%d][%p] key[%s], data[%p]\n", ptr_list_tuple->index, ptr_list_tuple,  ptr_list_tuple->key, ptr_list_tuple->vals.val_custom);
+                  break;
+              }
             }
-            if( ptr_tuple->hash_next == root_tuple ){
+            if( ptr_list_tuple->hash_next == root_tuple ){
                 break;
             }
-            ptr_tuple = ptr_tuple->hash_next;
+            ptr_list_tuple = ptr_list_tuple->hash_next;
         }
     }
 }
--- test_hmap.c
+++ test_hmap.c
@@ -3,160 +3,62 @@
 #include <string.h>
 #include <sys/time.h>
 #include "hmap.h"
-//#include "watch.h"
-
 
 int main(){
-    char *key1[] = {
-                    "version",
-                    "priority",
-                    "ds3politedegree",
-                    "expirygraceperiod",
-                    "preexpirythr",
-                    "postexpirythr",
-                    "lifecycleunit",
-                    "lifecycleno",
-                    "recurringlifecycle",
-                    "ds3timeresolution",
-                    "counterrollover",
-                    "refillstopext",
-                    "ds3packagedesc",
-                    "ds3offlinenotifpolicycontrol",
-                    "ds3recurringpolicycontrol",
-                    "ds3costcode",
-                    "persistcycle",
-                    "offerstate",
-                    "sponsorid",
-                    "privateidlist",
-                    "publicidlist",
-                    "implicitregsetid",
-                    "regprivateidlist",
-                    "registrationstatus",
-                    "aliaid",
-                    "pfpassword",
-                    "ds3pin",
-                    "secretquestion",
-                    "secretanswer",
-                    "accountid",
-                    "productnolist",
-                    "productownerlist",
-                    "mmtelprofileid",
-                    "addifexist",
-                    "old_number",
-                    "new_number",
-                    "ds3_polite_degree",
-                    "ds3_brandid",
-                    "ds3_spname",
-                    "ds3_registration_status",
-                    "language",
-                    "ivr_language",
-                    "sms_language",
-                    "ussd_language",
-                    "email_language",
-                    "preferences",
-                    "ds3_customer_category",
-                    "ds3_customer_subcategory",
-                    "ds3_customer_segment",
-                    "address",
-                    "birthday",
-                    "priority1",
-                    "version1",
-                    "sendsms",
-                    "locked",
-                    "activation_date",
-                    "counter_state",
-                    "expiry_time",
-                    "expiry_grace_period",
-                    "preexpiry_thr",
-                    "postexpiry_thr",
-                    "expiry_notify_time",
-                    "persist_cycle",
-                    "lifecycle_unit",
-                    "lifecycle_no",
-                    "recurring_lifecycle",
-                    "ds3_time_resolution",
-                    "counter_rollover",
-                    "refill_stop_time",
-                    "refill_stop_ext",
-                    "refill_notify_time",
-                    "amf_cost_code",
-                    "amf_dynamic_biz_info",
-                    "amf_order_date",
-                    "amf_recurring_state",
-                    "amf_package_refkey",
-                    "ds3_package_desc",
-                    "ds3_offline_notify_policy_control",
-                    "ds3_recurring_notify_policy_control",
-                    "ds3_cost_code",
-                    "sponsor_id",
-                    "offer_state",
-                    "last_index_name",
-                    "ok1"
-                };
     HMAP_DB *my_hmap_db = NULL;   
-    //struct timeval elap_tm;
-    int i = 0;
-    int data_len = 0; 
+    TUPLE *ptr_tuple    = NULL;
+    int data_len        = 0; 
+    int ret             = 0;
     char data[1024];
-    //char error[1024];
-    int ret = 0;
-    
-    //init_watch(TMR0, error);
-    hmap_init(1024, &my_hmap_db);
-    //stop_watch(TMR0, &elap_tm, error);
-    //printf("\x1b[41m<<<<< init_watch elap time: %lu.%.6lu >>>>>\x1b[0m\n", elap_tm.tv_sec, elap_tm.tv_usec);
-     
-    //init_watch(TMR0, error);
-    for(; i < 84 ; i++){
-        data_len = sprintf(data, "my data at %d", i);
-        if( (ret = hmap_add(&my_hmap_db, key1[i], strlen(key1[i]), data, data_len, 1)) < 0 ){
-            printf("Add error[%d] key[%s]\n", ret, key1[i]);
-            return -1; 
-        }
-    }
-    //stop_watch(TMR0, &elap_tm, error);
-    //printf("\x1b[41m<<<<< add data elap time: %lu.%.6lu >>>>>\x1b[0m\n", elap_tm.tv_sec, elap_tm.tv_usec);
-     
-    hmap_print_list(my_hmap_db);
-    if( (ret = hmap_delete(&my_hmap_db, key1[75], strlen(key1[75]))) != HMAP_SUCCESS){
-        printf("Delete error[%d]\n", ret);
-        return -1;
-    }
-    printf("AFTER DELETE:\n");
-    hmap_print_list(my_hmap_db);
-    
-    getchar();
-    data_len = sprintf(data, "my data at %d\n", 75);
-    hmap_add(&my_hmap_db, key1[75], strlen(key1[75]), data, data_len, 1);
-    printf("ADD AGAIN:\n");
-    hmap_print_list(my_hmap_db);
-    
-    getchar();
-    hmap_print_tree(my_hmap_db);
-    
-    printf( "before database tuple: %d\n", my_hmap_db->tuple_count);
-    if( (ret = hmap_delete(&my_hmap_db, key1[4], strlen(key1[4]))) != HMAP_SUCCESS){
-        printf("Delete error[%d]\n", ret);
-        return -1;
-    }
-    
-    printf( "after database tuple: %d\n", my_hmap_db->tuple_count);
-    
-    getchar();
-    hmap_print_tree(my_hmap_db);
 
-    
     struct my_data{
-        int a;
-        };
+    int a;
+    };
+
+    char *key[2]        = {"key:value1","key:value2"};
+
+    /* initialize database */
+    hmap_init(1024, &my_hmap_db);
+
+    /* add key="key:value2" value="kv2" */
+    data_len = sprintf(data, "kv2");
+    ret = hmap_add(&my_hmap_db, key[1], strlen(key[1]), 1, HMAP_DATA_TYPE_CHARS, data, data_len);
+
+    /* display database */
+    hmap_print_list(my_hmap_db);
+
+    /* add key="key:value2" value=@my_data */
     struct my_data *mdata = (struct my_data*) calloc(1, sizeof(struct my_data));
     mdata->a = 11;
-    ret = hmap_add_tuple_with_data(&my_hmap_db, key1[4], strlen(key1[4]), (void*)mdata, 1);
-    TUPLE *ptr_tuple = NULL;
-    ret = hmap_search(my_hmap_db, key1[4], strlen(key1[4]), &ptr_tuple);
-    if( ret == HMAP_SUCCESS )
-    printf("Try to search %s : %d\n", key1[4], ((struct my_data*)ptr_tuple->data)->a);
+    ret = hmap_add(&my_hmap_db, key[0], strlen(key[0]), 1, HMAP_DATA_TYPE_CUSTOM, (void*)mdata,0);
+
+    /* search key="key:value1" expect=1  */
+    ret = hmap_search(my_hmap_db, key[0], strlen(key[0]), &ptr_tuple);
+    printf("Try to search %s : %d\n", key[0], ((struct my_data*)ptr_tuple->vals.val_custom)->a);
+
     
+    hmap_add(&my_hmap_db, "age", strlen("age"), 1, HMAP_DATA_TYPE_UINT, 27, 0);
+    hmap_add(&my_hmap_db, "FirstName", strlen("FirstName"), 1, HMAP_DATA_TYPE_CHARS, "Narongsak", strlen("Narongsak"));
+    hmap_add(&my_hmap_db, "LastName", strlen("LastName"), 1, HMAP_DATA_TYPE_CHARS, "Mala", strlen("Mala"));
+    hmap_add(&my_hmap_db, "Weight", strlen("Weight"), 1, HMAP_DATA_TYPE_DOUBLE, 62.239);
+
+
+    /* display database */
+    hmap_print_list(my_hmap_db);
+
+    hmap_search(my_hmap_db, "FirstName", strlen("FirstName"), &ptr_tuple);
+    printf("First Name is   : %s\n", ptr_tuple->vals.val_chars);
+
+    hmap_search(my_hmap_db, "LastName", strlen("LastName"), &ptr_tuple);
+    printf("Last Name is    : %s\n", ptr_tuple->vals.val_chars);
+
+    hmap_search(my_hmap_db, "age", strlen("age"), &ptr_tuple);
+    printf("Ags             : %u years old\n", ptr_tuple->vals.val_uint);
+
+    hmap_search(my_hmap_db, "Weight", strlen("Weight"), &ptr_tuple);
+    printf("Weight          : %f kgs\n", ptr_tuple->vals.val_double);
+
+    /* destroy database */
     hmap_destroy(&my_hmap_db);
     
     return 0;
